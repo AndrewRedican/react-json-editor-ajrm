a899038e607e1fb6b203da3e885ca235e1e767e7
Andrew Redican <andrew.redican.mejia@gmail.com>
2018-05-28 18:59:53 -0600

    updated example

--- example/src/index.js
+++ example/src/index.js
@@ -19,49 +19,18 @@ class App extends Component {
         super(props);
     } 
     render() {
-
-        /**
-         * If no colors property is passed to this component, it'll use
-         * this darkTheme as default color pallette.
-         * (Which by the way, iy looks a lot like VSCode dark theme, I am a big fan!)
-         */
-        const darkTheme = {
-            default            : '#D4D4D4',
-            background         : '#1E1E1E',
-            background_warning : '#1E1E1E',
-            string             : '#CE8453',
-            number             : '#B5CE9F',
-            colon              : '#49B8F7',
-            keys               : '#9CDCFE',
-            keys_whiteSpace    : '#AF74A5',
-            primitive          : '#6392C6'
-        };
-
-        /**
-         * Another "light" theme that you can use.
-         * Basically you can create your own theme that fits your style.
-         */
-        const lightTheme = {
-            default            : '#D4D4D4',
-            background         : '#E3E9EA1A',
-            background_warning : '#f443361A',
-            string             : '#FA7921',
-            number             : '#70CE35',
-            colon              : '#49B8F7',
-            keys               : '#59A5D8',
-            keys_whiteSpace    : '#835FB6',
-            primitive          : '#386FA4'
-        };
-        
         /**
          * Rendeing this JSONInput component with some properties
          */
         return(
             <div style = {{ maxWidth: '1400px', maxHeight: '100%' }} >
                 <JSONInput
-                    id          = {'uniqueString-jsonInut'}
-                    placeholder = { sampleData }
-                    colors      = { darkTheme }
+                    id          = 'unique_string' //an id this is required
+                    placeholder = {sampleData}    //data to display initially
+                    theme       = 'light_mitsuketa_tribute'
+                    colors      = {{
+                        string : '#DAA520' // <-- this overrides a string's theme color with whatever color value you want (must be hexcode)
+                    }}
                     height      = '550px'
                 />
             </div>
