fb66543e9433f79af181fe05c9a88287df5f680c
Patrick Sachs <kontakt@patrick-sachs.de>
2018-07-31 23:51:10 +0200

    Polyfill in commonJS build, leave code largely untouched in ES build

--- babel.config.js
+++ babel.config.js
@@ -4,48 +4,55 @@ console.log("Running Babel ...", { BABEL_ENV });
 
 const moduleSystem = (BABEL_ENV && BABEL_ENV.startsWith('modules:')) ? BABEL_ENV.substring("modules:".length) : "es";
 
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          ie: 11,
-          edge: 14,
-          firefox: 45,
-          chrome: 49,
-          safari: 10,
-          node: '6.11',
-        },
-        modules: moduleSystem === "es" ? false : moduleSystem,
+// For the ES configuration only transpile react to valid JavaScript.
+// For commonjs transpile to old JS versions.
+const presets = moduleSystem === "es"
+  ? ['@babel/preset-react']
+  : [
+    ['@babel/preset-env', {
+      targets: {
+        ie: 11,
+        edge: 14,
+        firefox: 45,
+        chrome: 49,
+        safari: 10,
+        node: '6.11',
       },
-    ],
+      modules: moduleSystem,
+    }],
     '@babel/preset-react'
-  ],
+  ];
+
+// The ES system does not polyfill etc, while the others do.
+const transformOptions = moduleSystem === "es"
+  ? {
+    helpers: true,
+    polyfill: false,
+    useBuiltIns: true,
+    useESModules: true
+  }
+  : {
+    helpers: true,
+    polyfill: true,
+    useBuiltIns: false,
+    useESModules: false
+  };
+
+module.exports = {
+  presets,
   plugins: [
     '@babel/plugin-transform-object-assign',
     '@babel/plugin-proposal-object-rest-spread',
-    ['@babel/plugin-transform-runtime', { polyfill: false, useBuiltIns: true }],
+    ['@babel/plugin-transform-runtime', transformOptions]
   ],
   env: {
-    development: {
-      plugins: [
-        [
-          'module-resolver',
-          {
-            alias: {
-              modules: './modules',
-            },
-          }
-        ],
-      ],
-    },
     production: {
       plugins: [
+        // Optimize constant react elements in the production build. (Makes debugging harder, so skip this in development)
         'transform-react-constant-elements',
       ],
-      ignore: ['test/*'],
-    },
+      ignore: ['test/*']
+    }
   },
-  ignore: ['scripts/*.js'],
+  ignore: ['scripts/*.js']
 };
