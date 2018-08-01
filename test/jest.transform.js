13932c88e711de9031520263fb149b128aadf1ce
Andrew Redican <andrew.redican.mejia@gmail.com>
2018-08-01 13:14:00 -0600

    fixed import/export issues of test

--- test/jest.transform.js
+++ test/jest.transform.js
@@ -3,15 +3,15 @@ module.exports = require('babel-jest').createTransformer({
         '@babel/preset-env',
         '@babel/preset-react',
     ],
-    'plugins': [
+    plugins: [
         '@babel/plugin-transform-modules-commonjs',
         '@babel/plugin-proposal-object-rest-spread',
         'babel-plugin-extensible-destructuring',
         ['@babel/plugin-transform-runtime',{
-            'helpers': true,
-            'polyfill': true,
-            'useBuiltIns': false,
-            'useESModules': true
+            helpers      : true,
+            polyfill     : true,
+            useBuiltIns  : false,
+            useESModules : false
         }]
     ]
   });
