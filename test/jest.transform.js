63f7940facf546d7d9cb9abf9db0b2a79697df88
Andrew Redican <andrew.redican.mejia@gmail.com>
2018-08-02 13:10:01 -0600

    remove strict mode on tests babel config to be able to use err framework

--- test/jest.transform.js
+++ test/jest.transform.js
@@ -1,3 +1,9 @@
-const babel = require("../babel.config");
+let babel = require('../babel.config');
+
+babel.plugins.push(
+    ["@babel/plugin-transform-modules-commonjs", {
+        strictMode: false
+    }]
+);
 
 module.exports = require('babel-jest').createTransformer(babel);
