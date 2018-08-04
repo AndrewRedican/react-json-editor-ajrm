c4b4710dfaca02cb06c1684da73818782fe992e3
Patrick Sachs <kontakt@patrick-sachs.de>
2018-08-04 20:13:23 +0200

    Added tests for locale system

--- test/logic/index.js
+++ test/logic/index.js
@@ -1,7 +1,9 @@
 import syntaxTest from "./syntax";
+import localeTest from "./locale";
 
 function run() {
   syntaxTest();
+  localeTest();
   // ... Add more logic specifc tests once we need them in different files ...
 }
 
