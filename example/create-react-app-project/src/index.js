cd56931d2438efe1da736764f94bd334d7ee81dc
Markus Petrykowski <github@markus-petrykowski.de>
2018-08-30 09:42:57 +0200

    add create react app example

---
+++ example/create-react-app-project/src/index.js
@@ -0,0 +1,62 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// todo: use the imported version in prod, source in dev.
+/**
+ * Import RJEA component
+ */
+
+//Using distribution version in node_modules
+//import JSONInput  from 'react-json-editor-ajrm';
+//import locale     from 'react-json-editor-ajrm/locale/en';
+
+//Using distribution version from project
+//import JSONInput  from '../../dist';
+//import locale     from '../../dist/locale/en';
+
+//Using source code
+import JSONInput from "react-json-editor-ajrm/index";
+import locale from "react-json-editor-ajrm/locale/en";
+
+/**
+ * Import some data. This is a sample object, which will be passed down to JSONInput placeholder properperties.
+ * You can use placeholder to show data once, after component has mounted.
+ */
+import sampleData from "./sampledata";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    /**
+     * Rendering this JSONInput component with some properties
+     */
+    return (
+      <div style={{ maxWidth: "1400px", maxHeight: "100%" }}>
+        <JSONInput
+          id="unique_string" // an id is required
+          placeholder={sampleData} // data to display
+          theme="light_mitsuketa_tribute"
+          locale={locale}
+          colors={{
+            string: "#DAA520" // overrides theme colors with whatever color value you want
+          }}
+          height="550px"
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
