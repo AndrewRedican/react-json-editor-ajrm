7e9b63353e9148dd425bef9952e66698f20b863a
AndrewRedican <andrew.redican.mejia@gmail.com>
2020-10-15 08:55:23 +0100

    republish

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,9 +6,11 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
-## [2.5.12] - 2020-10-15
+## [2.5.13] - 2020-10-15
 ### Fix
-- Rebuilt project to fix import issue introduced on version `2.5.10`. @andrewredican
+- Re-published project to fix import issue introduced on version `2.5.10`. @andrewredican
+
+## [2.5.12] - 2020-10-15 - DO NOT USE
 
 ## [2.5.11] - 2020-10-12
 ### Added
--- example/webpack-project/src/index.js
+++ example/webpack-project/src/index.js
@@ -8,16 +8,16 @@ import "./index.css";
  */
 
 //Using distribution version in node_modules
-//import JSONInput  from 'react-json-editor-ajrm';
-//import locale     from 'react-json-editor-ajrm/locale/en';
+import JSONInput from 'react-json-editor-ajrm';
+import locale from 'react-json-editor-ajrm/locale/en';
 
 //Using distribution version from project
-//import JSONInput  from '../../../dist';
-//import locale     from '../../../dist/locale/en';
+// import JSONInput from '../../../dist';
+// import locale from '../../../dist/locale/en';
 
 //Using source code
-import JSONInput from "../../../src";
-import locale from "../../../src/locale/en";
+// import JSONInput from "../../../src";
+// import locale from "../../../src/locale/en";
 
 /**
  * Import some data. This is a sample object, which will be passed down to JSONInput placeholder properperties.
